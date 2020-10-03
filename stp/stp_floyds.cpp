b2ab3d31f783976b8cda9e77c0ee81d202fd7448
Ivan Donat Pupovac <ivandonat@gmail.com>
2020-10-03 14:49:55 +0100

    bugfix

--- stp/stp_floyds.cpp
+++ stp/stp_floyds.cpp
@@ -1,28 +1,8 @@
 // This program utilises Floyd-Warshall's Algorithm to solve STP
 
 #include <vector>
-#include <map>
-
 #include <iostream>
-
-/*
-struct Graph {
-    int num_of_nodes;
-    // edges : i -> (j -> a_ij)
-    std::map<int, std::map<int, int> > edges;
-};
-
-void print_graph(const Graph& graph) {
-    for(int i = 0; i < graph.num_of_nodes; i++) {
-        std::cout << "node " << i << std::endl;
-        std::map<int, int> edges_from_node = graph.edges.find(i)->second;
-        for(auto edge : edges_from_node) {
-            // edge-map of this node
-            std::cout << edges_from_node.size() << std::endl;
-        }
-    }
-}
-*/
+#include <algorithm>
 
 struct Graph {
     int num_of_nodes;
@@ -38,17 +18,22 @@ void print_graph(const Graph& graph) {
     } 
 }
 
-int main() {
+std::vector< std::vector<int> > inf_but_diagonal(int n) {
+    std::vector< std::vector<int> > ret;
+    ret.resize(n);
+    for(int i = 0; i < n; i++)
+        ret[i].resize(n, 1e9);
+    for(int i = 0; i < n; i++)
+        ret[i][i] = 0;
+    return ret;
+}
+
+Graph create_graph_from_stdin() {
     Graph g;
-    
     std::cin >> g.num_of_nodes;
 
-    // init edge matrix to zeros
-    g.edges.resize(g.num_of_nodes);
-    for(int i = 0; i < g.num_of_nodes; i++) {
-        g.edges[i].resize(g.num_of_nodes, 0);
-    }
-
+    g.edges = inf_but_diagonal(g.num_of_nodes);
+    
     // read edge inputs
     int num_of_edges;
     std::cin >> num_of_edges;
@@ -56,12 +41,43 @@ int main() {
         int i, j, a_ij, b_ij;
         std::cin >> i >> j >> a_ij >> b_ij;
         
-        g.edges[i][j] = a_ij;
-        g.edges[j][i] = -b_ij;
+        g.edges[i][j] = b_ij;
+        g.edges[j][i] = -a_ij;
     }
 
+    return g;
+}
 
+Graph generate_d_graph(const Graph& graph) {
+    int N = graph.num_of_nodes;
+    std::vector< std::vector<int> > E = inf_but_diagonal(N);
+
+    for(int i = 0; i < N; i++)
+        E[i][i] = 0;
+    
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            E[i][j] = graph.edges[i][j];
+
+    // tighten
+    for(int k = 0; k < N; k++) {
+    for(int i = 0; i < N; i++) {
+    for(int j = 0; j < N; j++) {
+        E[i][j] = std::min(
+            E[i][j],
+            E[i][k] + E[k][j]
+        );
+    }}}
+     
+    return {N, E};
+}
+
+int main() {
+    Graph g = create_graph_from_stdin();
     print_graph(g);
     
+    Graph d_graph = generate_d_graph(g);
+    print_graph(d_graph);
+
     return 0;
 }
